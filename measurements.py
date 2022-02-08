b95193e768c5b588762ebf176ec7f95c7be12fe7
Denis Vivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-08 12:14:12 +0200

    feat: add bernoulli trials using hacker statistics

--- measurements.py
+++ measurements.py
@@ -1,5 +1,9 @@
+from cmath import sqrt
+from typing import List
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from tqdm import tqdm
 
 from searching_algorithms.binary_search import BinarySearch
@@ -8,20 +12,70 @@ from searching_algorithms.interpolation_search import InterpolationSearch
 from sorting_algorithms.insertion_sort import InsertionSort
 from timer import Timer
 
+sns.set()
+sns.set_theme()
 np.random.seed(42)
 sizes = [10, 10**2, 10**3]
 measurements = {key: [] for key in sizes}
 algorithms = [BinarySearch, ExponentialSearch, InterpolationSearch]
 timer = Timer()
 
-for size in tqdm(sizes):
-    print(f"### SIZE: {size}")
-    array = np.random.randint(0, 100, size=size)
-    searching_value = np.random.choice(array)
-    sorted_array = InsertionSort(array).get_result()   
-    for algorithm in algorithms: 
+array = np.random.randint(0, 100000000, size=10**9)
+
+def ecdf(array):
+    x = np.sort(array)
+    y = np.arange(1, len(array) + 1) / len(array)
+    _ = plt.plot(x, y, marker=".", linestyle="none")
+
+
+def bernoulli_trial(array: List[int], size: int = 10, slice: int = 10) -> List[int]:
+    first_algorithm = []
+    second_algorithm = []
+    searching_elements = [np.random.choice(array[:slice]) for _ in range(size)]
+    for element in searching_elements:
         timer.start_measuring()
-        algorithm(sorted_array, searching_value)
-        print(f"{timer.stop_measuring()%60:f}s")
+        BinarySearch(array, element)
+        first_algorithm.append(timer.stop_measuring())
+        timer.start_measuring()
+        ExponentialSearch(array, element)
+        second_algorithm.append(timer.stop_measuring())
+
+    return [np.array(first_algorithm), np.array(second_algorithm)]
+
+compare = lambda x: np.sum(x[0] > x[1])
+# compare2 = lambda x: np.sum(x[0] < x[1])
+
+binomial_distribution = np.array([compare(bernoulli_trial(array, size=100, slice=len(array))) for _ in range(1000)])
+binomial_distribution2 = np.array([compare(bernoulli_trial(array, size=100, slice=10)) for _ in range(1000)])
+
+# records = bernoulli_trial(array, size=100)
+
+# print(records[0])
+# # _ = plt.hist(records[0], bins=10)
+# _ = plt.hist(records[1])
+# plt.show()
+# , density=True, stacked=True
+
+_ = sns.kdeplot(binomial_distribution)
+_ = sns.kdeplot(binomial_distribution2)
+_ = plt.xlabel("number of successes")
+_ = plt.ylabel("probability")
+print(f"{np.sum(binomial_distribution > 50) / len(binomial_distribution)}")
+plt.show()
+
+
+# ecdf(binomial_distribution)
+# ecdf(binomial_distribution2)
+
+
+# for size in tqdm(sizes):
+#     print(f"### SIZE: {size}")
+#     array = np.random.randint(0, 100, size=size)
+#     searching_value = np.random.choice(array)
+#     sorted_array = InsertionSort(array).get_result()   
+#     for algorithm in algorithms: 
+#         timer.start_measuring()
+#         algorithm(sorted_array, searching_value)
+#         print(f"{timer.stop_measuring()%60:f}s")
 
 
