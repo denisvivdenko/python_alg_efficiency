bcf2e158c60c9e5b9279e025cb0d5080133d7a1a
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-22 13:53:13 +0200

    feat: add recursive dot_product algorithm

---
+++ iterative_and_recursive_algorithms/recursion.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def dot_product(array: List[int], product: int = 1, next_element_index: int = 0) -> int:
+    '''
+        Multiplies all non-zero elements of the given array
+    '''
+
+    # termination condition
+    if len(array) == next_element_index:
+        return product
+
+    if array[next_element_index] == 0:
+        return dot_product(array, product, next_element_index + 1)
+    
+    return dot_product(array, product * array[next_element_index], next_element_index + 1)
+
+if __name__ == "__main__":
+    array = [2, 0, 3, 4, 0]
+    print(dot_product(array))
+    
+# output: 24
